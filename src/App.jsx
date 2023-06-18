0d6c7e2cb78a421e57f6c2fb19a1de94dfe30396
Kamal0078k <saikamal0078@gmai.com>
2023-06-18 11:08:20 +0530

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-import { useState } from "react";
+import react, { useState, useEffect } from "react";
 import Intro from "./Intro/Intro";
 import Body from "./Body";
 
 function App() {
+  useEffect(() => {
+    // Disable scrolling
+    document.body.style.overflow = "hidden";
+    const enableScrolling = () => {
+      document.body.style.overflow = "auto";
+    };
+    const timeout = setTimeout(enableScrolling, 3500);
+
+    const handleBeforeUnload = () => {
+      window.scrollTo(0, 0);
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      clearTimeout(timeout);
+    };
+    //
+
+    // // Enable scrolling after 3 seconds
+    //
+
+    //
+
+    // // Clean up the timeout on unmount
+    // return () => ;
+  }, []);
   return (
     <>
       <Intro />
